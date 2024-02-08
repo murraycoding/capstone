48bc5bbb2d7c47847d4389efce0f5e5396da4c1e
Brian Murray <murraycoding@gmail.com>
2024-02-07 20:06:57 -0500

    Add styles and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 // page imports
 
 // components imports
+import NavLink from './components/NavLink';
 
 // style imports
 import './App.css'
@@ -13,7 +14,14 @@ export default function App() {
 
   return (
     <>
-      <p>This is the header of the app.</p>
+      <header>
+      <h1>This is the header of the app.</h1>
+      <ul id="nav-bar">
+        <NavLink />
+      </ul>
+
+      </header>
+
       <Outlet />
       <p>This is the footer of the app</p>
 
