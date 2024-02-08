e6419247a1ae65e4c9c477e73b958eec1d3d76c5
Brian Murray <murraycoding@gmail.com>
2024-02-07 21:22:22 -0500

    Added header and footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                Brian Murray - January 2024 Cohort
+                <a id="email" href="mailto:bmurray5@travelers.com">bmurray5@travelers.com</a>
+            </footer>
+        </>
+    )
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import umbrella from '../assets/Umbrella_Color.png'
+
+export default function Title() {
+    return (
+        <div id="page-title">
+            <h1>Brian Murray</h1>
+            <img id="top-image" src={umbrella}/>
+            <h1>EDP Capstone Project</h1>
+        </div>
+    )
+}
+
