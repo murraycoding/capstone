2cd9aa1b33adce9889219d96b7da0ee6c55ad508
Brian Murray <murraycoding@gmail.com>
2024-02-08 21:23:37 -0500

    Fixed conditional styling on task card.

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,6 +1,6 @@
 export default function TaskCard({taskId, description, completionStatus, personAssigned, projectAssigned, dueDate, estimatedComplexity}){
     return (
-        <div className="task-card">
+        <div className={completionStatus == 'true' ? 'task-card task-complete' : 'task-card task-not-complete'}>
             <p>Task ID {taskId}</p>
             <p>Description = {description}</p>
             <p>Completion Status = {completionStatus}</p>
